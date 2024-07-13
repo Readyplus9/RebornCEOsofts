8b0761abf4e4ea7ed08c4524b0c89e691fe9ee89
Readyplus9 <readyplus9@gmail.com>
2024-07-13 15:48:58 -0700

    13 july 2024/2

---
+++ BIN/ceosofts-web/src/componants/Header.js
@@ -0,0 +1,16 @@
+//วิธีที่1
+// function Header() {
+//   return <h1>CEO softs from file Header</h1>;
+// }
+// export default Header;
+
+//วิธีที่2
+// export default function Header() {
+//   return <h1>CEO softs from file Header</h1>;
+// }
+
+//วิธีที่3
+const Header = () => {
+  return <h1>CEO softs from file Header</h1>;
+};
+export default Header;
